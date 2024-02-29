c0be2ca40ac7734f044f63cac07b6389d6dd0889
LCFone <putilov.k@gmail.com>
2024-02-29 20:39:34 +0200

    06pb

---
+++ src/components/redux/phoneBookReducer.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  contacts: [],
+  filter: '',
+};
+
+const phoneBookSlice = createSlice({
+  name: 'phoneBook',
+  initialState,
+  reducers: {
+    deleteContact: (state, action) => {
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+    addContact: (state, action) => {
+      state.contacts = [...state.contacts, action.payload];
+    },
+    onFilterChange: (state, action) => {
+      state.filter = action.payload;
+    },
+  },
+});
+
+export const phoneBookReducer = phoneBookSlice.reducer;
+export const { deleteContact, addContact, onFilterChange } =
+  phoneBookSlice.actions;
